281c30f322a5bc874e37916bcd696b004cd83712
Jovy Ong <jovy83@live.com>
2018-11-22 20:45:54 +0800

    lesson 04

---
+++ app/src/main/java/com/digigames_interactive/smack/CreateUserActivity.kt
@@ -0,0 +1,25 @@
+package com.digigames_interactive.smack
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar(view: View) {
+
+    }
+
+    fun generateColorClicked(view: View) {
+
+    }
+
+    fun createUserBtnClicked(view: View) {
+    }
+}
