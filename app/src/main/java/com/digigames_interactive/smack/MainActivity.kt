319fb912f8791eaf9780d5fb6d5215f5bb9ea8bc
Jovy Ong <jovy83@live.com>
2018-11-22 15:11:11 +0800

    lesson-02

--- app/src/main/java/com/digigames_interactive/smack/MainActivity.kt
+++ app/src/main/java/com/digigames_interactive/smack/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.digigames_interactive.smack
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -31,5 +31,17 @@ class MainActivity : AppCompatActivity() {
         } else {
             super.onBackPressed()
         }
+    }
+
+    fun loginBtnNavClicked(view: View) {
+
+    }
+
+    fun addChannelClicked(view: View) {
+
+    }
+
+    fun sendMsgBtnClicked(view: View) {
+        
     }
 }
