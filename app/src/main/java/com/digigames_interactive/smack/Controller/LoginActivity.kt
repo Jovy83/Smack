1e4f771f20a5ca4c55a1362fe0b7c1aeda763f3b
Jovy Ong <jovy83@live.com>
2018-11-26 10:05:35 +0800

    lesson 16

--- app/src/main/java/com/digigames_interactive/smack/Controller/LoginActivity.kt
+++ app/src/main/java/com/digigames_interactive/smack/Controller/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.digigames_interactive.smack.Controller
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import com.digigames_interactive.smack.R
 import com.digigames_interactive.smack.Services.AuthService
 import kotlinx.android.synthetic.main.activity_login.*
@@ -13,19 +16,32 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        loginSpinner.visibility = View.INVISIBLE
     }
 
     fun loginLoginBtnClicked(view: View) {
+        enableSpinner(true)
         val email = loginEmailTextField.text.toString()
         val password = loginPasswordTextField.text.toString()
+        hideKeyboard()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill all the fields.", Toast.LENGTH_SHORT).show()
+            return
+        }
 
         AuthService.loginUser(this, email, password) { loginSuccess ->
             if (loginSuccess) {
                 AuthService.findUserByEmail(this) { findSuccess ->
                     if (findSuccess) {
+                        enableSpinner(false)
                         finish()
+                    } else {
+                        errorToast()
                     }
                 }
+            } else {
+                errorToast()
             }
 
         }
@@ -35,5 +51,28 @@ class LoginActivity : AppCompatActivity() {
         val createUserIntent = Intent(this, CreateUserActivity::class.java)
         startActivity(createUserIntent)
         finish()
+    }
+
+    fun errorToast() {
+        Toast.makeText(this, "Something went wrong, please try again.", Toast.LENGTH_SHORT).show()
+        enableSpinner(false)
+    }
+
+    fun enableSpinner(enable: Boolean) {
+        if (enable) {
+            loginSpinner.visibility = View.VISIBLE
+        } else {
+            loginSpinner.visibility = View.INVISIBLE
+        }
+        loginLoginBtn.isEnabled = !enable
+        loginCreateUserBtn.isEnabled = !enable
+    }
+
+    fun hideKeyboard() {
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if (inputManager.isAcceptingText) {
+            // windowToken will grab which ever input method is active, in this case, the keyboard
+            inputManager.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+        }
     }
 }
